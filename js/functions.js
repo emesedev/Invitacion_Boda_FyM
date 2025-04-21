8063c58aa70d7f567e7d019ebb1e24478c82645f
emesedev <100376668+emesedev@users.noreply.github.com>
2025-04-21 17:34:27 -0600

    v 2.1.0

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,18 @@ function reproducirAudio() {
         audio.pause();
     }
 }
+
+function openModal() {
+    document.getElementById('myModal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+}
+
+window.onclick = function (event) {
+    const modal = document.getElementById('myModal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
